b422f058e9b36c3e7b7af86885666837b8d7dde1
Kirk Vander Meulen <kirk.vandermeulen@gmail.com>
2019-04-03 19:02:32 -0500

    created CCBounder class to include both EndRangeProbs. Also moved some general code up to EndRangeProb.

--- ghseqdb/cctable.py
+++ ghseqdb/cctable.py
@@ -4,20 +4,31 @@ from kmslib.hmmerkools import hmmsearchparser
 from . import seqdbutils
 
 class EndRangeProb:
-    def __init__(self,dpm,seqlen,hmmsize):
+    def __init__(self,hmmsize,conf_envfrac,extrange):
         self.hmmpositions=None
         self.seqpositions=None
         self.envfractions=None
         self.initial_logistic=None
         self.updated_prob=None
+        self.env_frac_remaining=None
         self.extrange=int(0.26*hmmsize)
+        self.envconf_term=1.0-conf_envfrac
+    def build_initial_logistic(self):
+        gamma=1
+        fx=-7.5*self.envfractions*np.exp(1-self.env_frac_remaining)
+        for x in range(len(self.envfractions)):
+            if self.envfractions[x]<=- self.envconf_term*self.env_frac_remaining:
+                fx[x]+=-50*(self.envfractions[x]+ self.envconf_term*self.env_frac_remaining)
+        self.initial_logistic=1/(1+np.exp(-gamma*(fx)))
+        self.updated_prob=self.initial_logistic.copy()
+
     def update_prob(self,curatedEA):
         pass
 
 ##HMMER numbering starts at 1!!!
 class NTRangeProb(EndRangeProb):
-    def __init__(self,dpm,seqlen,hmmsize):
-        super().__init__(dpm,seqlen,hmmsize)
+    def __init__(self,dpm,seqlen,hmmsize,conf_envfrac,extrange):
+        super().__init__(hmmsize,conf_envfrac,extrange)
         #HMMER is 1-indexed so if aln starts at first position ntalign_min,ntalign_max=0,1
         ntalign_min=max(0, (dpm.align_start-dpm.hmm_start)-self.extrange)
         ntalign_max=dpm.align_start 
@@ -27,19 +38,19 @@ class NTRangeProb(EndRangeProb):
         #keeping HMMER positions 1-indexed
         self.hmmpositions=np.array(range(dpm.hmm_start+1-ntalign_span,dpm.hmm_start+1))
         self.envfractions= -(self.hmmpositions-1)/hmmsize
+        self.env_frac_remaining=(dpm.hmm_start-1)/hmmsize
+        self.build_initial_logistic()
+        self.set_ctccbound()
 
-        gamma=1
-        env_frac_remaining=(dpm.hmm_start-1)/hmmsize
-        fx=-7.5*self.envfractions*np.exp(1-env_frac_remaining)
-        for x in range(len(self.envfractions)):
-            if self.envfractions[x]<=-0.05*env_frac_remaining:
-                fx[x]+=-50*(self.envfractions[x]+0.05*env_frac_remaining)
-        self.initial_logistic=1/(1+np.exp(-gamma*(fx)))
+    def set_ctccbound(self):
+        ccstart_index=np.where(self.updated_prob>=0.5)[0][0]
+        self.seq_ccbound=self.seqpositions[ccstart_index]
+        self.hmm_ccbound=self.hmmpositions[ccstart_index]
 
 ##HMMER numbering starts at 1!!!
 class CTRangeProb(EndRangeProb):
-    def __init__(self,dpm,seqlen,hmmsize):
-        super().__init__(dpm,seqlen,hmmsize)
+    def __init__(self,dpm,seqlen,hmmsize,conf_envfrac,extrange):
+        super().__init__(hmmsize,conf_envfrac,extrange)
         #HMMER is 1-indexed so if aln stops at last position ntalign_min,ntalign_max=seqlen-1,seqlen
         ctalign_min=dpm.align_stop-1
         ctalign_max=min(seqlen, dpm.align_stop + (hmmsize-dpm.hmm_stop) + self.extrange)
@@ -49,16 +60,26 @@ class CTRangeProb(EndRangeProb):
         #keeping HMMER positions 1-indexed
         self.hmmpositions=np.array(range(dpm.hmm_stop,dpm.hmm_stop+ctalign_span))
         self.envfractions=-((hmmsize-self.hmmpositions)/hmmsize)
-        
-        gamma=1
-        env_frac_remaining=(hmmsize-dpm.hmm_stop)/hmmsize
-        fx=-7.5*self.envfractions*np.exp(1-env_frac_remaining)
-        for x in range(len(self.envfractions)):
-            if self.envfractions[x]<=-0.05*env_frac_remaining:
-                fx[x]+=-50*(self.envfractions[x]+0.05*env_frac_remaining)
-        self.initial_logistic=1/(1+np.exp(-gamma*(fx)))
-    def get_boundary(self):
-        pass
+        self.env_frac_remaining=(hmmsize-dpm.hmm_stop)/hmmsize
+        self.build_initial_logistic()
+        self.set_ntccbound()
+       
+    def set_ntccbound(self):
+        ccstop_index=np.where(self.updated_prob>=0.5)[0][-1]
+        self.seq_ccbound=self.seqpositions[ccstop_index]
+        self.hmm_ccbound=self.hmmpositions[ccstop_index]
+
+class CCBounder:
+    def __init__(self,dpm,seqlen,hmmsize,conf_envfrac=0.9,extrange=0.26):
+        self.ntrangeprob=NTRangeProb(dpm,seqlen,hmmsize,conf_envfrac,extrange)
+        self.ctrangeprob=CTRangeProb(dpm,seqlen,hmmsize,conf_envfrac,extrange)
+    def get_seqntccbound(self):
+        return self.ntrangeprob.seq_ccbound
+    def get_seqctccbound(self):
+        return self.ctrangeprob.seq_ccbound
+
+#    def get_boundary(self):
+#        pass
 
 def build_cctable(dbpath,hmmsearchfpath,pfamcode):#,seqfpath,seqformat='fasta'):
     hsrp=hmmsearchparser.HMMERSearchResParser()
@@ -87,7 +108,7 @@ def build_cctable(dbpath,hmmsearchfpath,pfamcode):#,seqfpath,seqformat='fasta'):
     absent_entries=list(set(pgbsraccs).difference(filteredHT.keys()) )
     print(f'{len(absent_entries)} entries in PROTEINGBS but not {os.path.basename(hmmsearchfpath)}')
     c.execute('''CREATE TABLE IF NOT EXISTS CCDATA (acc text, version text, pfamcode text,pfamvrsn int,\
-                seq_checksum, hmmsfname text, hmmsf_mtime int, spm glob, ntrp glob, ctrp glob)''')
+                seq_checksum, hmmsfname text, hmmsf_mtime int, spm glob, ccb glob, ccstart int, ccstop int)''')
     #seq_checksum included so could compare from PROTEINGBS table perspective whether need to re-run hmmsearch 
     c.execute('''SELECT acc FROM CCDATA''')
     #find accs that need to be added-
@@ -108,11 +129,13 @@ def build_cctable(dbpath,hmmsearchfpath,pfamcode):#,seqfpath,seqformat='fasta'):
         c.execute('''SELECT * FROM PROTEINGBS WHERE acc = (?)''',(pacc,) )
         gbdbentry=c.fetchone()
         seqlen=len(pickle.loads(gbdbentry['pklgbsr']).seq)
-        ntrp=NTRangeProb(spm.dpms_[0],seqlen,spm.psize)
-        ctrp=CTRangeProb(spm.dpms_[0],seqlen,spm.psize)
+        ccbounder=CCBounder(spm.dpms_[0],seqlen,spm.psize)
         new_tuple=(pacc,pvrsn,pfamcode,spm.paccvrsn,gbdbentry['seq_checksum'],os.path.basename(hmmsearchfpath),\
-                   hmmsfile_mtime,pickle.dumps(spm),pickle.dumps(ntrp),pickle.dumps(ctrp))
-        c.execute('''INSERT INTO CCDATA VALUES (?,?,?,?,?,?,?,?,?,?)''',new_tuple)
+                   hmmsfile_mtime,pickle.dumps(spm),pickle.dumps(ccbounder),ccbounder.ntrangeprob.seq_ccbound,\
+                   ccbounder.ntrangeprob.seq_ccbound)
+        print(ccbounder.ntrangeprob.seq_ccbound,ccbounder.ctrangeprob.seq_ccbound)
+        c.execute('''INSERT INTO CCDATA VALUES (?,?,?,?,?,?,?,?,?,?,?)''',new_tuple)
+        ##ADD something that indicates coverage at start???
     conn.commit()
     conn.close()
     #ccvals=find_motifalign(my_motif)
--- ghseqdb/curatedtable.py
+++ ghseqdb/curatedtable.py
@@ -1,5 +1,13 @@
-import yaml
+import os
+import pandas as pd
+from . import seqdbutils
 
 
-def build_curatedtable(dbpath,cdyamlpath):
+def build_curatedxtaltable(dbpath,xtalxlfpath):
+    xtaldf=pd.read_excel(xtalfpath,....)
+    xtalfile_mtime=os.stat(xtalxlfpath).st_mtime
+
+    conn=seqdbutils.gracefuldbopen(dbpath)
+    c.execute('''CREATE TABLE IF NOT EXISTS CURATEDXTALS (acc text, chain text, xtalfname text,\
+                xtalf_mtime int, ntccpos int, ctccpos int, ntccseq text, ctccseq text)''')
     pass
